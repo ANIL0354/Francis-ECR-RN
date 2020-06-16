bda1c237e1f8d052ef5651556cdcd75131356acd
Aditi Singh <aditi.singh@chicmic.co.in>
2020-06-16 19:24:51 +0530

    -- ios keyboardAvoidingView issue fixed.

--- src/components/hoc/AppHoc/index.js
+++ src/components/hoc/AppHoc/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { View, StatusBar, SafeAreaView, Alert, Keyboard } from 'react-native';
+import { View, StatusBar, KeyboardAvoidingView, Platform, SafeAreaView, Alert, Keyboard } from 'react-native';
 import AppHeader from '../../atoms/AppHeader';
 import { stopLoader, logout } from '../../../redux/actions';
 import messaging from '@react-native-firebase/messaging';
@@ -59,56 +59,20 @@ const AppHoc = ({
   }, [])
 
   return (
-    <SafeAreaView style={styles.hocWrapper}>
-      <StatusBar backgroundColor="white" barStyle="dark-content" />
-      <AppHeader
-        rightIcon={rightIcon}
-        centerIcon={userToken && centerIcon ? centerIcon : null}
-        leftIcon={leftIcon}
-        userToken={userToken}
-        onLeftIconTap={() => {
-          if (fromSummary && !rideBooked) {
-            Alert.alert(fromSummary && !rideBooked ? 'Warning' : STRINGS.LOGOUT,
-              fromSummary && !rideBooked ? 'You will loose all your data. Are you sure, you want to logout?' : STRINGS.LOGOUT_DESCRIPTION, [
-              {
-                text: STRINGS.CANCEL,
-                onPress: () => { },
-              },
-              {
-                text: fromSummary ? "Yes, I'm sure" : STRINGS.CONFIRM,
-                onPress: () => logout(
-                  userToken,
-                  () => {
-                    if (fromSummary || rideBooked) {
-                      navigation.navigate(SCREENS.HOME);
-                    }
-                  },
-                  () => {
-                  },
-                ),
-              },
-            ]);
-          }
-          else {
-            navigation.navigate(SCREENS.HOME);
-          }
-        }}
-        onCenterIconTap={() => {
-          navigation.navigate(SCREENS.PROFILE);
-        }}
-        rightMenuItems={userToken
-          ? [
-            {
-              label: 'Your Trips',
-              onPress: () => navigation.navigate(SCREENS.YOUR_TRIPS),
-            },
-            {
-              label: 'Your Ratings',
-              onPress: () => navigation.navigate(SCREENS.YOUR_RATINGS),
-            },
-            {
-              label: STRINGS.LOGOUT,
-              onPress: () => Alert.alert(fromSummary && !rideBooked ? 'Warning' : STRINGS.LOGOUT,
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : 'height'}
+      style={{ flex: 1 }}
+    >
+      <SafeAreaView style={styles.hocWrapper}>
+        <StatusBar backgroundColor="white" barStyle="dark-content" />
+        <AppHeader
+          rightIcon={rightIcon}
+          centerIcon={userToken && centerIcon ? centerIcon : null}
+          leftIcon={leftIcon}
+          userToken={userToken}
+          onLeftIconTap={() => {
+            if (fromSummary && !rideBooked) {
+              Alert.alert(fromSummary && !rideBooked ? 'Warning' : STRINGS.LOGOUT,
                 fromSummary && !rideBooked ? 'You will loose all your data. Are you sure, you want to logout?' : STRINGS.LOGOUT_DESCRIPTION, [
                 {
                   text: STRINGS.CANCEL,
@@ -119,33 +83,74 @@ const AppHoc = ({
                   onPress: () => logout(
                     userToken,
                     () => {
-                      navigation.navigate(SCREENS.HOME);
+                      if (fromSummary || rideBooked) {
+                        navigation.navigate(SCREENS.HOME);
+                      }
                     },
                     () => {
                     },
                   ),
                 },
-              ]),
-            },
-          ]
-          : [
-            {
-              label: STRINGS.LOGIN_OR_SIGNUP,
-              onPress: () => {
-                stopLoader();
-                navigation.navigate(SCREENS.LOGIN, { fromDetails: false });
+              ]);
+            }
+            else {
+              navigation.navigate(SCREENS.HOME);
+            }
+          }}
+          onCenterIconTap={() => {
+            navigation.navigate(SCREENS.PROFILE);
+          }}
+          rightMenuItems={userToken
+            ? [
+              {
+                label: 'Your Trips',
+                onPress: () => navigation.navigate(SCREENS.YOUR_TRIPS),
               },
-            },
-          ]}
-      />
-      {children}
-      {loader && (
-        <View
-          style={styles.loaderWrapper}>
-          <CustomLoader stopLoader={stopLoader} loader={loader} />
-        </View>
-      )}
-    </SafeAreaView>
+              {
+                label: 'Your Ratings',
+                onPress: () => navigation.navigate(SCREENS.YOUR_RATINGS),
+              },
+              {
+                label: STRINGS.LOGOUT,
+                onPress: () => Alert.alert(fromSummary && !rideBooked ? 'Warning' : STRINGS.LOGOUT,
+                  fromSummary && !rideBooked ? 'You will loose all your data. Are you sure, you want to logout?' : STRINGS.LOGOUT_DESCRIPTION, [
+                  {
+                    text: STRINGS.CANCEL,
+                    onPress: () => { },
+                  },
+                  {
+                    text: fromSummary ? "Yes, I'm sure" : STRINGS.CONFIRM,
+                    onPress: () => logout(
+                      userToken,
+                      () => {
+                        navigation.navigate(SCREENS.HOME);
+                      },
+                      () => {
+                      },
+                    ),
+                  },
+                ]),
+              },
+            ]
+            : [
+              {
+                label: STRINGS.LOGIN_OR_SIGNUP,
+                onPress: () => {
+                  stopLoader();
+                  navigation.navigate(SCREENS.LOGIN, { fromDetails: false });
+                },
+              },
+            ]}
+        />
+        {children}
+        {loader && (
+          <View
+            style={styles.loaderWrapper}>
+            <CustomLoader stopLoader={stopLoader} loader={loader} />
+          </View>
+        )}
+      </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 };
 
